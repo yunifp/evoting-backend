c7dfbe65e2eaf2de2af8036368b2a401c90b6764
yunifp <darrenarmani666@gmail.com>
2026-04-28 12:30:45 +0700

    s

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,10 +17,11 @@ func main() {
 	config.ConnectDatabase()
 	seeders.RunSeeder(config.DB)
 	seeders.SeedDummyData(config.DB)
+	seeders.SeedLayanan(config.DB)
 
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://127.0.0.1:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Accept", "X-Requested-With"},
 		ExposeHeaders:    []string{"Content-Length"},
